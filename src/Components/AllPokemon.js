eaf32956002b5901f7d1461c1c7bcf7e57912b0b
Rauolli <rauol@t-online.de>
2022-10-05 02:36:36 +0200

    added fillUpNumber-Function to AllPokemon and changed CSS of OnePokemon

--- src/Components/AllPokemon.js
+++ src/Components/AllPokemon.js
@@ -4,8 +4,16 @@ import "../Styles/globalStyle.css";
 
 export default function AllPokemon(props) {
   const { id, name, type } = props;
-  const myType = props.type;
+  // const myType = props.type;
 
+  const fillUpNumber = (id)=>{
+    const idStr = id.toString();
+    let numberStr = "";
+    for (let i = 0; i < (3 - idStr.length); i++)  {
+      numberStr += "0";     
+    }
+    return `#${numberStr}${id}`;
+  }
   return (
     <>
       <Link to={`/Pokemon/${id}`}>
@@ -19,15 +27,15 @@ export default function AllPokemon(props) {
           <article className="text-container">
             <h2>{name.english}</h2>
             <div className="info">
-              {myType.length &&
-                myType.map((element) => {
+              {type.length &&
+                type.map((element) => {
                   return (
                     <span className={PokemonType(element)}>{element}</span>
                   );
                 })}
             </div>
 
-            <span>NR. {id}</span>
+            <span>{fillUpNumber(id)}</span>
           </article>
         </div>
       </Link>
